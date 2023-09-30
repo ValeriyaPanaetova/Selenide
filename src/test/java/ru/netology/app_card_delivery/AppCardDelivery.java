a8a51f3f78fcd8991d9b691ff2638d999ba92b8f
Valeriya <Lerusya.Pan@yandex.ru>
2023-09-30 20:50:52 +0700

    All my changes

---
+++ src/test/java/ru/netology/app_card_delivery/AppCardDelivery.java
@@ -0,0 +1,42 @@
+package ru.netology.app_card_delivery;
+
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class AppCardDelivery {
+    @BeforeEach
+    void setUp() {
+        open("http://localhost:9999");
+    }
+
+
+    @Test
+    public void shouldReturnSuccessIfFieldsAreFilledInCorrectly() {
+
+        LocalDate deliveryDateCard = LocalDate.now().plusDays(3);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String dateText = deliveryDateCard.format(formatter);
+
+        $("[data-test-id=city] [placeholder='Город']").setValue("Краснодар");
+        $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=date] [class='input__box'] [placeholder='Дата встречи']").setValue(dateText);
+        $("[data-test-id=name] [type=text]").setValue("Вася Пупкин");
+        $("[data-test-id=phone] [type=tel]").setValue("+79883339920");
+        $("[data-test-id=agreement]").click();
+        $("[role=button] .button__content").click();
+        $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofMillis(15000));
+        $("[data-test-id='notification'] .notification__content")
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + dateText));
+    }
+
+}
